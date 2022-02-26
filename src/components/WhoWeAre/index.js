913b0b2dff522bff17b4e00b64994eccd0532e1d
maris <marisandb@yahoo.com>
2022-02-26 01:11:41 -0600

    added content for who wer are page

--- src/components/WhoWeAre/index.js
+++ src/components/WhoWeAre/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import "./style.css"
+
+// import images
+import Kathy from '../../assets/staff/Kathy.jpg';
+import Katy from '../../assets/staff/Katy.jpg';
+import Casey from '../../assets/staff/Casey.jpg';
+
 
 function WhoWeAre() {
     return (
         <section>
-            Placeholder
+                <h1>Meet Our Certified Humane Officers</h1>
+            <div>
+                <img src={Kathy} alt="Kathy holding three baby owls" />
+                <p>
+                Lorem ipsum dolor sit amet. 33 voluptatem deserunt ut dolorem maxime et sunt ducimus rem ullam voluptate. Rem omnis aspernatur ab possimus deserunt in nulla nemo in soluta voluptas nam similique sint.
+                </p>
+            </div>
+            <div>
+                <img src={Katy} alt="Katy holding a baby owl" />
+                <p>
+                Lorem ipsum dolor sit amet. 33 voluptatem deserunt ut dolorem maxime et sunt ducimus rem ullam voluptate. Rem omnis aspernatur ab possimus deserunt in nulla nemo in soluta voluptas nam similique sint.
+                </p>
+            </div>
+            <div>
+                <img src={Casey} alt="Casey holding an eagle" />
+                <p>
+                Lorem ipsum dolor sit amet. 33 voluptatem deserunt ut dolorem maxime et sunt ducimus rem ullam voluptate. Rem omnis aspernatur ab possimus deserunt in nulla nemo in soluta voluptas nam similique sint.
+                </p>
+            </div>
         </section>
     )
 }
